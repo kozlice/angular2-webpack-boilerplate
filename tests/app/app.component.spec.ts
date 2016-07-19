bf8ec94848f7989308f6ad30b4f5fb3919bb4e57
Valentin Nazarov <rusarmy@gmail.com>
2016-07-19 19:04:53 +0300

    Update tests

--- tests/app/app.component.spec.ts
+++ tests/app/app.component.spec.ts
@@ -1,18 +1,10 @@
-import {
-    beforeEachProviders,
-    it,
-    inject,
-    expect
-} from '@angular/core/testing';
-
+import { addProviders, inject } from '@angular/core/testing';
 import { AppService } from '../../src/app/app.service';
 
 describe('App Service', () => {
-    beforeEachProviders(() => {
-        return [
-            AppService
-        ];
-    });
+    beforeEach(() => addProviders([
+        AppService
+    ]));
 
     it('should return title', inject([AppService], (appService) => {
         expect(appService.getTitle()).toBe('Hello, world!');
